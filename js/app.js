5f053add757bd68e3728dbf6ab0663d6893cda44
Shuric-git <ulme@rambler.ru>
2021-06-23 13:30:52 +0300

    add inner plugin

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
-const slides = document.querySelectorAll('.slide')
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
+function slidesPlugin (activeSlide = 4) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides [activeSlide].classList.add('active')
+    
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin(4)
